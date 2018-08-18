589bd99a34545205a143e91b6efeb7c5914d0f85
tehZevo <djevergreene@gmail.com>
2018-08-18 19:18:43 -0400

    ironing out initial bugs

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ module.exports = {
   GridWorld: require("./GridWorld.js"),
   Renderer: require("./Renderer.js"),
   envs: {
-    Collector = require("./Collector.js");
+    Collector = require("./Collector.js")
   }
 }
